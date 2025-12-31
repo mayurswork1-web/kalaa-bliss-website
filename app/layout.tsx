d39f53f649e07db78d90d5d355cb1c704f1eb423
Vaibhav Kamble <itsvaibhav.work@gmail.com>
2026-01-01 01:10:20 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 const geistSans = Geist({
@@ -35,6 +37,8 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <SpeedInsights />
+        <Analytics />
         <Footer />
       </body>
     </html>
